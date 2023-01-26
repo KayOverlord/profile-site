720672bd88657058dd905dd07ab52252895d6f8f
Kabelo Leboa <kavinleboa@gmail.com>
2023-01-26 14:03:48 +0200

    work projects update

--- src/data/slideData.js
+++ src/data/slideData.js
@@ -1,4 +1,4 @@
-import { BsLinkedin, BsGithub, BsBoxArrowUpRight } from "react-icons/bs";
+import { BsCircleFill, BsGithub, BsBoxArrowUpRight } from "react-icons/bs";
 import proPic from "../../src/assets/propic.jpg";
 export const Data = [
   {
@@ -33,12 +33,14 @@ inset 0 0 1.3rem purple`,
     color: "purple",
   },
   {
-    title: "I-Nav",
+    title: "i-Nav",
     description:
-      "This was a project I was working during my time at Ultrafrica",
-    info: `I've worked for 2 Companies so far and these are the technologies I used during my time with both of them.
-      React Native, React.js,Next.js, Node.js, Firebase, GraphQL,PostgreSQL,Docker.\n Okay let's see the Blow`,
-    image: "icon",
+      "This was a project I was working on during my time at Ultrafrica",
+    info: `i-Nav is a smartphone application used to provide navigation assistance for people living with disability. 
+    The app provides the users with an ultimate ability to independently navigate both outdoor and indoor environments. 
+    The app simplifies navigation by providing them with disability friendly access points surrounding their environments.`,
+    image:
+      "https://play-lh.googleusercontent.com/WqfMRd7AOwmctxgS8YmeI2XCfUNy1uWaNEMHj3c7zHuTpsgWOXHGkHuyYrgCcx8QDw=w240-h480-rw",
     iconName: (
       <BsGithub
         size={110}
@@ -55,7 +57,119 @@ inset 0 0 1.3rem red`,
     ),
     color: "red",
     link: (
-      <a href="https://www.linkedin.com/in/kabelo-leboa-6bb27365/">
+      <a href="https://play.google.com/store/apps/details?id=com.ttz.kmystro.i_nav&hl=en_ZA&gl=US">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "Checkers",
+    description:
+      "The first project I had my hands on during my time at Make Better Consulting",
+    info: `The Checkers app offers thousands of products and the ability of their customers to save (Xtra Savings) with highly personalised offers.`,
+    image:
+      "https://play-lh.googleusercontent.com/qR6RwYkdo9Js_NDz8h-1SUeI3oP2zazDeTGFKzXymwwd5lYGx9huatNKVF6tRQ9vOA=w240-h480-rw",
+    iconName: (
+      <BsGithub
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem red,
+0 0 0.8rem red,
+0 0 2.8rem red,
+inset 0 0 1.3rem red`,
+        }}
+      />
+    ),
+    color: "red",
+    link: (
+      <a href="https://play.google.com/store/apps/details?id=za.co.shoprite.eezicoupon&hl=en_ZA&gl=US">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "myTFGworld",
+    description:
+      "A project I had my hands on during my time at Make Better Consulting",
+    info: `The app offers a comprehensive portfolio of retail brands that include clothing, footwear, jewellery, sportswear, mobile phones, technology products and home furniture.`,
+    image:
+      "https://play-lh.googleusercontent.com/pKBc2qqNjCT_SK9W7cty5t9sU7ql-MpI2vHOI1XJoFla309YBVBT0Z6iC3G2_sIsgIqQ=w240-h480-rw",
+    iconName: (
+      <BsGithub
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem red,
+0 0 0.8rem red,
+0 0 2.8rem red,
+inset 0 0 1.3rem red`,
+        }}
+      />
+    ),
+    color: "red",
+    link: (
+      <a href="https://play.google.com/store/apps/details?id=com.tfgbeta&hl=en_ZA&gl=US">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "PetRock",
+    description:
+      "Another project I had my hands on during my time at Make Better Consulting",
+    info: `Pet Rock is a private platform for digital asset investments, offering regulated access to crypto markets.`,
+    image:
+      "https://uploads-ssl.webflow.com/62ff7f8952ca1007ee5b5af0/63060533e1e0b82e7c8c089d_petrockLogoInvertColour.svg",
+    iconName: (
+      <BsGithub
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem red,
+0 0 0.8rem red,
+0 0 2.8rem red,
+inset 0 0 1.3rem red`,
+        }}
+      />
+    ),
+    color: "red",
+    link: (
+      <a href="https://www.petrockinvest.com/">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "Ovex",
+    description:
+      " Yep!!, during my time at Make Better Consulting",
+    info: `A platform that offers cryptocurrency trading and the largest selection of digital asset markets. I Was part of the team helping to develop the Ovex app.`,
+    image:
+      "icon",
+    iconName: (
+      <BsCircleFill
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem red,
+0 0 0.8rem red,
+0 0 2.8rem red,
+inset 0 0 1.3rem red`,
+        }}
+      />
+    ),
+    color: "red",
+    link: (
+      <a href="https://www.ovex.io/">
         <BsBoxArrowUpRight size={35} color="red" />
       </a>
     ),
