a79b9896a550fdd50c759186c41c59a342774b9f
Kabelo Leboa <kavinleboa@gmail.com>
2023-01-26 14:41:27 +0200

    projects update

--- src/data/slideData.js
+++ src/data/slideData.js
@@ -1,4 +1,10 @@
-import { BsCircleFill, BsGithub, BsBoxArrowUpRight } from "react-icons/bs";
+import {
+  BsCircleFill,
+  BsGithub,
+  BsBoxArrowUpRight,
+  BsFillBriefcaseFill,
+} from "react-icons/bs";
+import { GiSwitchWeapon } from "react-icons/gi";
 import proPic from "../../src/assets/propic.jpg";
 export const Data = [
   {
@@ -17,7 +23,7 @@ export const Data = [
       ReactNative, React.js,Next.js, Node.js, Firebase, GraphQL,PostgreSQL,Docker.\n Okay let's see the Blow`,
     image: "icon",
     iconName: (
-      <BsGithub
+      <BsFillBriefcaseFill
         size={110}
         style={{
           borderRadius: "50%",
@@ -148,14 +154,13 @@ inset 0 0 1.3rem red`,
   },
   {
     title: "Ovex",
-    description:
-      " Yep!!, during my time at Make Better Consulting",
+    description: " Yep!!, during my time at Make Better Consulting",
     info: `A platform that offers cryptocurrency trading and the largest selection of digital asset markets. I Was part of the team helping to develop the Ovex app.`,
-    image:
-      "icon",
+    image: "icon",
     iconName: (
       <BsCircleFill
         size={110}
+        color={"black"}
         style={{
           borderRadius: "50%",
           boxShadow: ` 0 0 .2rem #fff,
@@ -177,11 +182,11 @@ inset 0 0 1.3rem red`,
   {
     title: "Personal Projects",
     description: "These are the my personal projects",
-    info: `A web developer is a programmer who specializes in, or is
-      specifically engaged in, the development of World Wide Web`,
+    info: `As a developer you need time to yourself so you can sharpen your skills, like a young prodigy trying to avenge the death of a sansei
+    in a 90s action flick`,
     image: "icon",
     iconName: (
-      <BsGithub
+      <GiSwitchWeapon
         size={110}
         style={{
           borderRadius: "50%",
@@ -195,5 +200,89 @@ inset 0 0 1.3rem green`,
       />
     ),
     color: "green",
+  },
+  {
+    title: "Goli",
+    description:
+      " Goli was a project I build to explore the world of blockchain technology ",
+    info: `Goli is Polygon Token build to power software applications and innovative technologies through funding.`,
+    image:
+      "https://scontent.fjnb1-1.fna.fbcdn.net/v/t39.30808-6/302602781_403379111922365_3113053652681245952_n.png?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGLRv6ycgkA06qeL28vYrOiOtVDd8ZKCA061UN3xkoIDZqLxPHypPMTE-WbIBSoWZTeNCuqFDzP2-HY8Kv0OfbR&_nc_ohc=He6IJlBbkjoAX_lLK2F&_nc_zt=23&_nc_ht=scontent.fjnb1-1.fna&oh=00_AfCzB4uLBrllD0F9eWRUNxiWu0ASUjNle3OO8637U1uTkw&oe=63D740EC",
+    iconName: (
+      <BsCircleFill
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem yellow,
+0 0 0.8rem yellow,
+0 0 2.8rem yellow,
+inset 0 0 1.3rem yellow`,
+        }}
+      />
+    ),
+    color: "yellow",
+    link: (
+      <a href="https://golinetwork.com/">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "CatchOne",
+    description:
+      " CatchOne Introduced me to the world of Maps & location data via Google Maps ",
+    info: `Catch1 focuses on providing the latest information on the transportation industry from the ground up and who is best to deliver that information then the people who use those public roads. 
+    Therefore the app relies on information given to it by the community on the app.`,
+    image:
+      "https://play-lh.googleusercontent.com/1nPd0Z0kb_dHFttDzLNABbmO5QOPNZ97kmvudTurM1dS1PTOFAKfCjKG8qfZ7G1y1jWE=w240-h480-rw",
+    iconName: (
+      <BsCircleFill
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem yellow,
+0 0 0.8rem yellow,
+0 0 2.8rem yellow,
+inset 0 0 1.3rem yellow`,
+        }}
+      />
+    ),
+    color: "yellow",
+    link: (
+      <a href="https://play.google.com/store/apps/details?id=com.ttz.kmystro.catchone">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
+  },
+  {
+    title: "TopThat",
+    description: "TopThat allowed me to put the Goli Token to use and to try expo pwa",
+    info: `TopThat is a story-sharing platform. For each story users will be given points, these points will be used to determine how much Goli tokens users get. Goli tokens will be sent to users each month. The TopThat algorithm will distribute Goli tokens to the users crypto wallet(which is created during the signup process)`,
+    image:
+      "https://play-lh.googleusercontent.com/aZBrnFHIMEnqnAKoGuQfQN3iJrRNUGwqM5nGQGPkV0vaKmu303ZNcZu1atlnvDv2yGk=w240-h480-rw",
+    iconName: (
+      <BsCircleFill
+        size={110}
+        style={{
+          borderRadius: "50%",
+          boxShadow: ` 0 0 .2rem #fff,
+0 0 .2rem #fff,
+0 0 2rem yellow,
+0 0 0.8rem yellow,
+0 0 2.8rem yellow,
+inset 0 0 1.3rem yellow`,
+        }}
+      />
+    ),
+    color: "yellow",
+    link: (
+      <a href="https://topthatstory.com/">
+        <BsBoxArrowUpRight size={35} color="red" />
+      </a>
+    ),
   },
 ];
